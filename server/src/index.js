915fe820afc56594b149c0c7da718ee018b51b0f
Noah Huppert <contact@noahh.io>
2021-03-29 00:42:01 -0400

    wip adding mongodb to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const mongodb = require("mongodb");
 
-const port = process.env.PORT || 8000;
+const cfg = {
+  httpPort: process.env.PORT || 8000,
+  mongoURI: process.env.MONGO_URI || "mongodb://dev-country-picker-example:dev-country-picker-example@127.0.0.1:27017",
+  mongoDBName: process.env.MONGO_DB_NAME || "dev-country-picker-example",
+};
+
+let startProms = [];
+
 const app = express();
 app.use(cors());
+
+const mongoClient = new mongodb.MongoClient(cfg.mongoURI, { useUnifiedTopology: true });
+let db = null;
+startProms.push(
+  mongoClient.connect().then(() => {
+    db = mongoClient.db(cfg.mongoDBName);
+  })
+);
 
 app.get("/api/v0/health", (req, res) => {
   res.json({
@@ -28,6 +44,17 @@ app.get("/api/v0/name/:query", async (req, res) => {
   }));
 });
 
-app.listen(port, () => {
-  console.log(`HTTP server listening on ${port}`);
-});
+(async function() {
+  try { 
+    await Promise.all(startProms);
+  } catch (e) {
+    console.error("Failed to complete pre-start task:", e);
+    return;
+  }
+
+  app.listen(cfg.httpPort, () => {
+    console.log(`HTTP server listening on ${cfg.httpPort}`);
+  });
+
+  mongoClient.close();
+})()
